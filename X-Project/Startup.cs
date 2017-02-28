591e0073069dcaae60beb88fb80ece75bafcd03c
Antonio Mirkovic <antoniomirkovic@outlook.com>
2017-02-28 22:05:18 +0100

    add project

---
+++ X-Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(X_Project.Startup))]
+namespace X_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
